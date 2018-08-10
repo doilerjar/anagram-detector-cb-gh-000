aeb2b8d557313bec0a7b8334c0df21524b5263f6
Kevin K <doilerjar@outlook.com>
2018-08-10 08:38:29 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,15 @@
 # Your code goes here!
 class Anagram
   
-  def self.new()
+  attr_accessor :anagram_word
   
+  def self.new(word)
+    @anagram_word = word.reverse
+    self
+  end 
+  
+  def self.match(word_list)
+    
   end 
   
 end 
